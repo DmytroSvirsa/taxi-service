bc511db490ac161371d310dfb9a9bf8eb39235c3
Dima Svirsa <august4smile@gmail.com>
2022-07-08 18:38:22 +0300

    no heroku import

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,7 +145,6 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
